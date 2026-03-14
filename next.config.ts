51aafea2ec281d03bfc2fe215c4a39e3cc214b1e
Lim-ZY <tcmpiano03@gmail.com>
2026-03-14 23:08:30 +0800

    Add support to link notes

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  trailingSlash: true,
+
+  async rewrites() {
+    return [
+      {
+        source: '/notes',
+        destination: `${process.env.QUARTZ_URL}/`,
+      },
+      {
+        source: '/notes/:path*',
+        destination: `${process.env.QUARTZ_URL}/:path*`
+      },
+    ];
+  },
 };
 
 export default nextConfig;
